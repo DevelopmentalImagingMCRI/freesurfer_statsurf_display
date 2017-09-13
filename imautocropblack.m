cb183a7ff17f0f6ba9091fea2cc8889a73f897cd
chrisadamsonmcri <chris.adamson@mcri.edu.au>
2017-09-13 11:17:53 +1000

    Use size(IMG, 3) not ndims(IMG)

--- imautocropblack.m
+++ imautocropblack.m
@@ -15,18 +15,18 @@ M = any(IMG > 0, 3);
 NewIMG = cell(3, 3);
 
 % put padding in the corners
-NewIMG{1, 1} = zeros([Padding, Padding, ndims(IMG)], 'uint8');
-NewIMG{3, 1} = zeros([Padding, Padding, ndims(IMG)], 'uint8');
-NewIMG{1, 3} = zeros([Padding, Padding, ndims(IMG)], 'uint8');
-NewIMG{3, 3} = zeros([Padding, Padding, ndims(IMG)], 'uint8');
+NewIMG{1, 1} = zeros([Padding, Padding, size(IMG, 3)], 'uint8');
+NewIMG{3, 1} = zeros([Padding, Padding, size(IMG, 3)], 'uint8');
+NewIMG{1, 3} = zeros([Padding, Padding, size(IMG, 3)], 'uint8');
+NewIMG{3, 3} = zeros([Padding, Padding, size(IMG, 3)], 'uint8');
 
 % put padding in the northern and southern parts
-NewIMG{1, 2} = zeros([Padding, size(IMG, 2), ndims(IMG)], 'uint8');
-NewIMG{3, 2} = zeros([Padding, size(IMG, 2), ndims(IMG)], 'uint8');
+NewIMG{1, 2} = zeros([Padding, size(IMG, 2), size(IMG, 3)], 'uint8');
+NewIMG{3, 2} = zeros([Padding, size(IMG, 2), size(IMG, 3)], 'uint8');
 
 % put padding in the western and eastern parts
-NewIMG{2, 1} = zeros([size(IMG, 1), Padding, ndims(IMG)], 'uint8');
-NewIMG{2, 3} = zeros([size(IMG, 1), Padding, ndims(IMG)], 'uint8');
+NewIMG{2, 1} = zeros([size(IMG, 1), Padding, size(IMG, 3)], 'uint8');
+NewIMG{2, 3} = zeros([size(IMG, 1), Padding, size(IMG, 3)], 'uint8');
 
 % put cropped image in the middle
 NewIMG{2, 2} = IMG(min(I):max(I), min(J):max(J), :);
