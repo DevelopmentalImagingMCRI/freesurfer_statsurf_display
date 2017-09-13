a7c9f59bb751e649eb6a67a7903a76c66fa1958f
chrisadamsonmcri <chris.adamson@mcri.edu.au>
2017-09-13 11:20:33 +1000

    Removed debugging

--- freesurfer_statsurf_savefig.m
+++ freesurfer_statsurf_savefig.m
@@ -2,8 +2,6 @@ function [varargout] = freesurfer_statsurf_savefig(PNGName)
 
 FigPos = get(gcf, 'Position');
 set(gcf, 'PaperPosition', FigPos, 'PaperUnits', 'points', 'InvertHardCopy', 'off');
-%keyboard;
-PNGName = fullfile(GHPages, 'main_dkt_fsrgb.png');
 exportfig(gcf, PNGName, 'Format', 'png', 'Width', FigPos(3), 'Height', FigPos(4), 'Color', 'rgb');
 
 IMG = imread(PNGName);
